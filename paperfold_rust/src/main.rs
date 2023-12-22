3cdb69befc544629a60c2b6bd09ba7073cd3d457
Frank Hillard <frank.hillard@gmail.com>
2023-12-22 12:12:00 +0100

    readme

--- paperfold_rust/src/main.rs
+++ paperfold_rust/src/main.rs
@@ -10,11 +10,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_init() {
+    fn test_initialization_10_5() {
         let p : paper::Paper = paper::Paper::initialize(10_u8, 5_u8);
         // println!("{}", p.width().to_string());
         assert_eq!(p.width(), 10_u8);
         assert_eq!(p.height(), 5_u8);
+    }
+
+    #[test]
+    fn test_init_fold_up() {
+        let mut p : paper::Paper = paper::Paper::initialize(10_u8, 5_u8);
+        p.fold(1, paper::Direction::UP);
+        println!("{:?}", p.data());
+        // assert_eq!(p.width(), 10_u8);
+        // assert_eq!(p.height(), 5_u8);
     }
 
 
